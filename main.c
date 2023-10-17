2ff923c40dfbf7f4d9b14ce25596c8ffe97a6175
oumaima <elghachiomaima1@gmail.com>
2023-10-17 13:11:55 +0100

    test 3

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0};
+/**
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
